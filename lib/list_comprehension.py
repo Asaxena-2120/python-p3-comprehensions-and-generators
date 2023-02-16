9ce454ee37d60e96f7b49f413c590d9323ef8400
Aastha <saxena.aastha91@gmail.com>
2023-02-17 04:04:54 +0530

    passed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-
+import ipdb
 def return_evens(num_list):
+    ans=[]
+    for i in num_list:
+        if i%2==0:
+            ans.append(i)
+    return ans
     pass
 
 def make_exclamation(sentence_list):
-    pass
+    ans=[]
+    if len(sentence_list)>0:
+        for sentence in sentence_list:
+            ans.append(sentence[:]+"!")
+        
+    
+    return ans
